d684cd93632bcb8e764edc86ab1899ac517e8ec2
Deredeld <oshea.norman@gmail.com>
2021-12-06 17:05:50 +0100

    Part 2 optimisation

--- Day 6/2.py
+++ Day 6/2.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import time
 
 #Get first line
 with open("input.txt", "r") as f:
@@ -8,18 +9,13 @@ with open("input.txt", "r") as f:
 	dic = {'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0}
 	dic.update(dict(Counter(numbers)))
 
+	start_time = time.time()
 	for i in range (256):
 		#Temp value to cycle values
-		temp = 0
-		temp2 = 0
-		for j in range(8,-1,-1):
-			if j==0:
-				temp = dic[str(j)]
-				dic[str(j)]=temp2
-				dic["6"]+=temp
-				dic["8"]=temp
-			else:
-				temp = dic[str(j)]
-				dic[str(j)]=temp2
-				temp2 = temp
+		temp = dic["0"]
+		for j in range(8):
+				dic[str(j)]=dic[str(j+1)]
+		dic["6"]+=temp
+		dic["8"]=temp
 	print(sum(dic.values()))
+	print("--- %s seconds ---" % (time.time() - start_time))
