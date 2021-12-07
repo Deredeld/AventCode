8031ef5d9d8f8fe9af58b45a828a1298be7ec0b7
Deredeld <oshea.norman@gmail.com>
2021-12-07 15:12:21 +0100

    Part 2 - Optimisation - Hill algorith

--- Day 7/2.py
+++ Day 7/2.py
@@ -1,6 +1,7 @@
 import statistics
 import math
 import time
+
 def CalculateCost(number,aim):
 	 distance = abs(number-aim)
 	 cost = 0
@@ -12,20 +13,22 @@ def CalculateCost(number,aim):
 with open("input.txt", "r") as f:
 	numbers = list(map(int,f.readline().replace("\n","").split(",")))
 	med = int(statistics.median(numbers))
-	bestCost = 0
-	for number in numbers:
-		bestCost+=CalculateCost(number,med)
+	bestCost = 9999999999999
 	start_time = time.time()
 
 	#CalculateForNextBest
+	position = []
+	Values = []
 	newMed = med
-	for i in range(med,max(numbers)):
+	for i in range(0,max(numbers)):
 		newCost = 0
 		for number in numbers:
 			newCost+=CalculateCost(number,i)
 		if newCost<bestCost:
 			bestCost=newCost
 			newMed=i
+		else:
+			break
 	print(bestCost)
-	print(i)
+	print(newMed)
 	print("--- %s seconds ---" % (time.time() - start_time))
